3c302476525cf42eb8f22648250dfc0412e57348
rahul kirpekar <rahul.kirpekar@gmail.com>
2023-03-26 09:18:06 +0530

    arrya

---
+++ src/basic/TestApp2.java
@@ -0,0 +1,24 @@
+package basic;
+
+import java.util.Scanner;
+
+public class TestApp2 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int a[] = new int[5];
+		
+		for (int i = 0; i < a.length; i++) 
+		{
+			System.out.println("Enter A[" + i + "] : ");
+			a[i] = sc.nextInt();
+		}
+		
+		for (int i = 0; i < a.length; i++) 
+		{
+			System.out.println("A["+i+"] : " + a[i]);
+		}
+	}
+}
